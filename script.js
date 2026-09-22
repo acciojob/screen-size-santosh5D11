4c65a3e687a84748c4bddfe983cea9fa83442878
Motapandle Santosh <santoshmotapandle@gmail.com>
2026-09-22 23:47:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function updateSize() {
+    const sizeInfo = document.getElementById("sizeInfo");
+
+    sizeInfo.querySelector("h1").textContent =
+        `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
+
+window.addEventListener("resize", updateSize);
+
+updateSize();
